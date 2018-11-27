0300db1d9e0840588bea9815ab40bc043ef2b296
ggdog <lzx2150@gmail.com>
2018-11-28 00:27:05 +0800

    版本1.0.0 Signed-off-by: ggdog <lzx2150@gmail.com>

---
+++ Plugin.php
@@ -0,0 +1,110 @@
+<?php
+if (!defined('__TYPECHO_ROOT_DIR__')) exit;
+
+/**
+ * Typecho文章静态化插件
+ *
+ * @package TpHtmlCache
+ * @author huhaku
+ * @version 1.0.0
+ * @link http://www.phpgao.com
+ */
+class TpHtmlCache_Plugin implements Typecho_Plugin_Interface
+{
+    public static function activate()
+    {
+        //页面收尾
+        Typecho_Plugin::factory('index.php')->begin = array('TpHtmlCache_Plugin', 'Start');
+        Typecho_Plugin::factory('index.php')->end = array('TpHtmlCache_Plugin', 'Ends');
+        return '插件安装成功';
+    }
+
+    /**
+     * 禁用插件方法,如果禁用失败,直接抛出异常
+     *
+     * @static
+     * @access public
+     * @throws Typecho_Plugin_Exception
+     */
+    public static function deactivate()
+    {
+    }
+
+    /**
+     * 获取插件配置面板
+     *
+     * @access public
+     * @param Typecho_Widget_Helper_Form $form 配置面板
+     * @return void
+     */
+    public static function config(Typecho_Widget_Helper_Form $form)
+    {
+		
+    }
+    /**
+     * 个人用户的配置面板
+     *
+     * @access public
+     * @param Typecho_Widget_Helper_Form $form
+     * @return void
+     */
+    public static function personalConfig(Typecho_Widget_Helper_Form $form)
+    {
+    }
+
+    /**
+     * 判断入口
+     */
+    public static function Start()
+    {
+        //已登录用户不缓存
+        if(Typecho_Widget::widget('Widget_User')->hasLogin()) return '';
+		if(self::needCache($_SERVER["REQUEST_URI"])){
+			return '';
+			}
+		else{
+			$expire = 2592000;
+			$file=__DIR__."/cache/".md5($_SERVER["REQUEST_URI"]).".html";//路径
+			if (file_exists($file)) {
+				$file_time = @filemtime($file);
+				if(time()-$file_time<$expire){
+					echo file_get_contents($file);//直接输出缓存
+					exit();
+				}else{
+				ob_start();//打开缓冲区
+				}
+			} else {
+				ob_start();//打开缓冲区
+			}
+		}
+		
+    }
+	
+    /**
+     * 缓存操作
+     */
+    public static function Ends()
+    {
+	if(Typecho_Widget::widget('Widget_User')->hasLogin()) return '';
+	if(self::needCache($_SERVER["REQUEST_URI"])){
+		return '';
+		}else{
+	$file=__DIR__."/cache/".md5($_SERVER["REQUEST_URI"]).".html";//路径
+	$html=ob_get_contents()."<!--TpHtmlCache ".date("Y-m-d h:i:s")."-->";
+		file_put_contents($file,$html);
+		}
+    }
+	
+	/**
+     * 根据配置判断是否需要缓存
+     * @param string 路径信息
+     * @return bool
+     */
+    public static function needCache($path)
+    {
+        if(strstr($path,'/posts/')) 
+			return '0';
+		else
+			return '1';
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# TpHtmlCache
-typecho文章静态化缓存插件
+# Typecho 文章静态化缓存插件 TpHtmlCache
+
+## 插件简介
+
+没钱买数据库，还在用着阿里最渣档次的ecs，得想办法优化下加载速度先<br>
+<br>
+激活插件后所有文章会在本地以静态页的方式被缓存掉<br>
+写的比较急，功能还比较简单，还没写后台和设置，就这样先把,剩下后面慢慢再完善成一个插件<br>
+
+## 安装方法
+
+1. 到https://github.com/huhaku/typecho_TpHtmlCache 项目中下载最新版本的文件。<br>
+2. 将下载的压缩包进行解压，重命名为`TpHtmlCache`,修改66行的缓存时间（$expire）105行的需要缓存的路径(/posts/),并上传至`Typecho`插件目录中。<br>
+3. 后台激活插件。<br>
+
+已测试可用typecho版本 1.1 (17.10.30)<br>
+
+## 注意
+
+1. cache目录必须为可写
+
